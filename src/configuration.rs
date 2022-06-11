0ac90781869f9affe0c9636dac07c081792970d6
zakiafada32 <afadazaki32@gmail.com>
2022-06-11 13:53:49 +0700

    configuration module

---
+++ src/configuration.rs
@@ -0,0 +1,42 @@
+#[derive(serde::Deserialize, Debug)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize, Debug)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+use config::Config;
+
+impl Settings {
+    pub fn get_configuration() -> Result<Self, config::ConfigError> {
+        let settings = Config::builder()
+            .add_source(config::File::with_name("configuration.yaml"))
+            .build()?;
+
+        settings.try_deserialize()
+    }
+}
+
+impl DatabaseSettings {
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
+        )
+    }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
+}
