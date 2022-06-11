0104f8d9813ba913f6e80bd022e08af7169914e0
zakiafada32 <afadazaki32@gmail.com>
2022-06-11 13:55:08 +0700

    connect db and subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
+use subscription::configuration::Settings;
 use subscription::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000")?;
-    run(listener)?.await
+    let configuration = Settings::get_configuration().expect("Failed to load configuration");
+    let db_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to database");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)?;
+    run(listener, db_pool)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +9,24 @@ pub struct FormData {
     email: String,
 }
 
-pub async fn subsribes(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subsribes(form: web::Form<FormData>, db_pool: web::Data<PgPool>) -> HttpResponse {
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(db_pool.get_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to insert subscription: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
