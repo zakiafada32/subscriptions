dcada022f250f5fee3f8c4477ea982553fd899f0
zakiafada32 <afadazaki32@gmail.com>
2022-06-12 14:29:10 +0700

    add telemetry module

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use subscription::configuration::Settings;
 use subscription::startup::run;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use subscription::telemetry::{get_subscriber, init_subcriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -14,16 +11,8 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to database");
 
-    LogTracer::init().expect("Failed to initialize log tracer");
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("subscription".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set global default subscriber");
+    let subscriber = get_subscriber("subscription".into(), "info".into());
+    init_subcriber(subscriber);
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
