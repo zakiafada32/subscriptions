82b7cd2b2bc95a6346c54f97a294663ba6b363ec
zakiafada32 <afadazaki32@gmail.com>
2022-06-19 23:33:11 +0700

    env layer - local - production
    sqlx check

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,9 +1,33 @@
+use config::Config;
 use secrecy::{ExposeSecret, Secret};
-
 #[derive(serde::Deserialize, Debug)]
 pub struct Settings {
     pub database: DatabaseSettings,
     pub application_port: u16,
+}
+
+impl Settings {
+    pub fn get_configuration() -> Result<Self, config::ConfigError> {
+        let base_path = std::env::current_dir().expect("failed to get current directory");
+        let configuration_directory = base_path.join("configuration");
+        let environment: Environment = std::env::var("APP_ENVIRONMENT")
+            .unwrap_or_else(|_| "local".into())
+            .try_into()
+            .expect("APP_ENVIRONMENT");
+
+        let settings = Config::builder()
+            // base environment
+            .add_source(config::File::from(configuration_directory.join("base")).required(true))
+            // layer on specific environment
+            .add_source(
+                config::File::from(configuration_directory.join(environment.as_str()))
+                    .required(true),
+            )
+            .add_source(config::Environment::with_prefix("app").separator("__"))
+            .build()?;
+
+        settings.try_deserialize()
+    }
 }
 
 #[derive(serde::Deserialize, Debug)]
@@ -13,18 +37,6 @@ pub struct DatabaseSettings {
     pub port: u16,
     pub host: String,
     pub database_name: String,
-}
-
-use config::Config;
-
-impl Settings {
-    pub fn get_configuration() -> Result<Self, config::ConfigError> {
-        let settings = Config::builder()
-            .add_source(config::File::with_name("configuration.yaml"))
-            .build()?;
-
-        settings.try_deserialize()
-    }
 }
 
 impl DatabaseSettings {
@@ -47,5 +59,34 @@ impl DatabaseSettings {
             self.host,
             self.port
         ))
+    }
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "Unknown environment: {}, use either 'local' or 'production'",
+                other
+            )),
+        }
     }
 }
